a3d943e5aeafe9ea4543d3d821e5621dd4b9c8ca
Rhuan Santos <rhuansantosdev@gmail.com>
2021-01-03 23:47:43 -0500

    uploading build files to github

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 This assessment was bespoke handcrafted for santos.
 
 Read more about this assessment [here](https://react.eogresources.com)
+
+#Live Demo: [here](https://rhuansantos.github.io/eog-react-assessment/)
+
+Note: This is my first time using Typescript and GraphQL, I do need to invest more time to learn the best practices on Typescript but I gave my best to complete this assesment in 1 day.
---
+++ docs/precache-manifest.1f71ebaf745122649777d941d1989e89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444a76f7b73249a6a5bb303346136469",
+    "url": "/eog-react-assessment/index.html"
+  },
+  {
+    "revision": "dce09c41ae64b97db612",
+    "url": "/eog-react-assessment/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "dce09c41ae64b97db612",
+    "url": "/eog-react-assessment/static/js/2.2615568f.chunk.js"
+  },
+  {
+    "revision": "a0734caf4a18ebdc59d8b0631c320a12",
+    "url": "/eog-react-assessment/static/js/2.2615568f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaccfeac38a22a7028b7",
+    "url": "/eog-react-assessment/static/js/main.e0efb4f7.chunk.js"
+  },
+  {
+    "revision": "40750af34d1b6a225996",
+    "url": "/eog-react-assessment/static/js/runtime-main.54a6e731.js"
+  }
+]);
